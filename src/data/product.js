f2412fd1f2778bf48e66a0723bc1bb3c3a4f9d62
Elvis <isabelguerra323@gmail.com>
2025-11-28 10:06:18 -0500

    product.js

---
+++ src/data/product.js
@@ -0,0 +1,52 @@
+export const products = [
+  {
+    id: 1,
+    name: "Libre Inversión",
+    min: 1000000,
+    max: 50000000,
+    tasa: 19,
+    plazo: 60,
+    image: "/images/Linversion.png",
+    description: "Uso libre para inversión o consumo. Proceso rápido y digital."
+  },
+  {
+    id: 2,
+    name: "Vehículo",
+    min: 3000000,
+    max: 80000000,
+    tasa: 13,
+    plazo: 84,
+    image: "/images/Car.png",
+    description: "Financiación para compra de vehículos nuevos o usados."
+  },
+  {
+    id: 3,
+    name: "Vivienda",
+    min: 10000000,
+    max: 300000000,
+    tasa: 9,
+    plazo: 240,
+    image: "/images/casa.png",
+    description: "Compra, mejora o construcción de vivienda con condiciones preferenciales."
+  },
+  {
+    id: 4,
+    name: "Educativo",
+    min: 500000,
+    max: 30000000,
+    tasa: 12,
+    plazo: 60,
+    image: "/images/estudios.png",
+    description: "Financiación para estudios, pregrado o posgrado."
+  },
+  {
+    id: 5,
+    name: "Empresarial",
+    min: 5000000,
+    max: 500000000,
+    tasa: 11,
+    plazo: 120,
+    image: "/images/Empresarial.png",
+    description: "Líneas para empresas y emprendedores con condiciones por volumen."
+  }
+];

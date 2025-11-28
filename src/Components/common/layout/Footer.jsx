b84ad5549fc596170bd78af9993ee68e11b48dbd
Elvis <isabelguerra323@gmail.com>
2025-11-28 10:13:57 -0500

    Transcripcion del footer

---
+++ src/Components/common/layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () => {
+return (
+    <footer className="site-footer">
+    <div className="container footer-inner">
+        <div>
+        <p className="muted">CreditSmart - Sistema de Gestión de Solicitudes de Crédito</p>
+        </div>
+        <div className="muted">© 2025 SivleTech Solutions S.A.S</div>
+    </div>
+    </footer>
+);
+};
+
